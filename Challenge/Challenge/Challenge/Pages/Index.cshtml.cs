68a74e0992620bc0cf74fbecb93aed45549fddd5
Matt <87787075+maty714@users.noreply.github.com>
2022-02-24 21:40:11 -0500

    Initial commit

---
+++ Challenge/Challenge/Challenge/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenge.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            DateTime utcTime = DateTime.UtcNow;
+            TimeZoneInfo tzi = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            DateTime localTime = TimeZoneInfo.ConvertTimeFromUtc(utcTime, tzi);
+            ViewData["ServerTime"] = localTime;
+        }
+    }
+}
---
+++ CodeFirst/CodeFirstApplication/CodeFirstApplication/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BlogContext())
+            {
+                Console.Write("Enter a name for a new blog:");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name, BlogId = 4 };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+            }
+        }
+        public class Blog
+        {
+            public int BlogId { get; set; }
+            public string Name { get; set; }
+
+            public virtual List<Post> Posts { get; set; }
+        }
+
+        public class Post
+        {
+            public int PostId { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+
+            public int BlogId { get; set; }
+            public virtual Blog Blog { get; set; }
+        }
+
+        public class BlogContext : DbContext
+        {
+            public DbSet<Blog> Blogs { get; set; }
+            public DbSet<Post> Posts { get; set; }
+        }
+    }
+}
